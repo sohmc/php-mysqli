9e7aa9fee9bb7baf739ee878771b16d3aa60683d
Michael Soh <sohmc@users.noreply.github.com>
2019-12-15 17:51:07 -0500

    Fixed plural

--- .travis-ci/current_tags.sh
+++ .travis-ci/current_tags.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -e ./php ]]; then
+    echo "Deleting old file..."
+    rm ./php
+fi
+
 PHP_TAGS_URL="https://github.com/docker-library/official-images/raw/master/library/php"
 
 echo "Downloading the latest tag file from ${PHP_TAGS_URL}"
@@ -13,6 +18,9 @@ sed -i 's/^Tags: //' ./php
 
 echo "Removing commas and adding newlines"
 sed -i 's/, /\n/g' ./php
+
+echo "Adding environment variable prefix"
+sed -i 's/^/  - TAG=/g' ./php
 
 echo "Tags ready:"
 cat ./php
